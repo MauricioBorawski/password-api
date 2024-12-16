3ac2840aaf7a0ce5e1d1c31c0491f1e107767008
Mauricio Borawski <mauricio.borawski@gmail.com>
2024-12-16 17:06:17 -0300

    create db

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => { });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
