774e86ad09753b9e546aaf4346dfd4f2ca599123
Mauricio Borawski <mauricio.borawski@gmail.com>
2025-01-03 16:40:04 -0300

    Create basic endpoints and add zod

--- controllers/user-controllers.ts
+++ controllers/user-controllers.ts
@@ -1,1 +1,51 @@
-import { Response, Request } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+import type { Response, Request } from 'express';
+
+const prisma = new PrismaClient();
+
+export async function getUsers(_req: Request, res: Response) {
+  const user = await prisma.user.findMany();
+
+  await prisma.$disconnect();
+
+  res.status(200).json(user);
+}
+
+export async function createUser(req: Request, res: Response) {
+  if (!req.body) {
+    res.status(400).json({ error: 'Request body is required' });
+
+    return;
+  }
+
+  if (Object.keys(req.body).length > 2) {
+    res
+      .status(400)
+      .json({ error: 'Request body should have only name and email' });
+
+    return;
+  }
+
+  if (
+    Object.keys(req.body).some((key) => {
+      return key !== 'name' && key !== 'email';
+    })
+  ) {
+    res
+      .status(400)
+      .json({ error: 'Request body should have only name and email' });
+
+    return;
+  }
+
+  if (req.body.name === '' || req.body.email === '') {
+    res.status(400).json({ error: 'Name and email are required' });
+
+    return;
+  }
+
+  const { email, name } = req.body;
+
+  res.status(201).json({ email, name });
+}
--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import userRoutes from './routes/users-routes';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
+
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
